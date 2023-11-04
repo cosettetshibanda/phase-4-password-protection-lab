c517d3de33985e85d4fa3f4691b20652145c6e7a
Cosette Tshibanda <cosettetshibanda@gmail.com>
2023-11-04 13:05:04 -0600

    commit tree e1f7387e1a6cf82b0726b9a01179d3105bfb227c

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :users
+  post "/login", to: "session#create"
+  get "/me", to: "users#show"
+  delete "/logout", to: "session#destroy"
+  post "/signup", to: "users#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
